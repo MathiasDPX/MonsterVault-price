b6906a89167051086a3e6cb9cba25c3b570c0d2f
MathiasDPX <56231137+MathiasDPX@users.noreply.github.com>
2024-04-06 11:53:11 +0200

    Add 4 hours every sessions
    1 sessions = 20 gems

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from math import ceil
 import json
 
 data = json.load(open('data.json', 'r', encoding='utf-8'))
 
+# Calculate the gems needed
 sum = 0
 for i in range(1, 1500):
     sum += data[str(i)]
@@ -17,6 +19,14 @@ def euclidean_division(dividend, divisor):
 
 ads_length = 40
 t = ads_length * sum
+
+"""
+Adds the 4 hours between each "sessions" (one session equals 20 gems)
+*not sure this is how it should be done but hey it's there
+
+session = ceil(sum / 20)
+t += (session * (4*3600))
+"""
 
 min, sec = euclidean_division(t, 60)
 h, min = euclidean_division(min, 60)
